9a320deaf528fdb31914329a6339c21fe394bfc1
ridhaaisa <ridhaaisaa@gmail.com>
2023-05-20 23:30:28 +0800

    Instance Object

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -11,7 +11,7 @@ package praktek01;
  */
 public class MahasiswaAksi {
     public static void main(String[] args){
-            
+            Mahasiswa Harry =new Mahasiswa();
             }
           
 }
